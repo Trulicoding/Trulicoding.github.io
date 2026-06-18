00e735f35c67cd9c46d23955bf7c04006c8cb282
Dallen Sandstrom <92828859+Trulicoding@users.noreply.github.com>
2026-06-18 04:28:13 -0600

    Updated to initial complete state

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-document.querySelectorAll(".glow-card").forEach(card => {
-    card.addEventListener("mousemove", event => {
-        const rect = card.getBoundingClientRect();
-
-        const x = event.clientX - rect.left;
-        const y = event.clientY - rect.top;
-
-        card.style.setProperty("--x", `${x}px`);
-        card.style.setProperty("--y", `${y}px`);
-    });
-});
-
 const sections = document.querySelectorAll("section");
 const navLinks = document.querySelectorAll("nav a");
 
